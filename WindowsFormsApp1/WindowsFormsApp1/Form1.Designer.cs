695859ae1d58f07d86154ccfa816d8ea72b809f7
dsvstryga <sdmytro111@gmail.com>
2026-03-30 17:21:30 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxArray = new System.Windows.Forms.TextBox();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btn2D = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введіть масив:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Результат:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Двовимірний масив";
+            // 
+            // textBoxArray
+            // 
+            this.textBoxArray.Location = new System.Drawing.Point(120, 19);
+            this.textBoxArray.Multiline = true;
+            this.textBoxArray.Name = "textBoxArray";
+            this.textBoxArray.Size = new System.Drawing.Size(158, 62);
+            this.textBoxArray.TabIndex = 3;
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(120, 118);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(158, 62);
+            this.textBoxResult.TabIndex = 4;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(203, 89);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Run";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btn2D
+            // 
+            this.btn2D.Location = new System.Drawing.Point(203, 194);
+            this.btn2D.Name = "btn2D";
+            this.btn2D.Size = new System.Drawing.Size(75, 23);
+            this.btn2D.TabIndex = 6;
+            this.btn2D.Text = "Run";
+            this.btn2D.UseVisualStyleBackColor = true;
+            this.btn2D.Click += new System.EventHandler(this.btn2D_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 552);
+            this.Controls.Add(this.btn2D);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.textBoxArray);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxArray;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btn2D;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn2D_Click(object sender, EventArgs e)
+        {
+            int[,] matrix = {
+        { 1, 2, 3 },
+        { -4, 5, 6 },
+        { 7, 8, -9 }
+    };
+
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            // Верхній правий
+            int topRight = matrix[0, cols - 1];
+
+            // Будь-який інший (наприклад центр)
+            int other = matrix[1, 1];
+
+            string result = "";
+
+            if (Math.Abs(topRight) > Math.Abs(other))
+                result += "Верхній правий більший по модулю\n";
+            else
+                result += "Інший більший по модулю\n";
+
+            // Порівняння двох будь-яких
+            int a = matrix[0, 0];
+            int b = matrix[2, 2];
+
+            if (a < b)
+                result += $"{a} менше за {b}";
+            else
+                result += $"{b} менше за {a}";
+
+            textBoxResult.Text = result;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // 1. Зчитуємо масив з TextBox
+            string[] parts = textBoxArray.Text.Split(' ');
+            double[] arr = parts.Select(double.Parse).ToArray();
+
+            // а) Сума елементів з непарними індексами
+            double sumOddIndex = 0;
+            for (int i = 1; i < arr.Length; i += 2)
+            {
+                sumOddIndex += arr[i];
+            }
+
+            // б) Сума між першим і останнім від’ємними
+            int firstNeg = -1, lastNeg = -1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    if (firstNeg == -1) firstNeg = i;
+                    lastNeg = i;
+                }
+            }
+
+            double sumBetween = 0;
+            if (firstNeg != -1 && lastNeg != -1 && firstNeg < lastNeg)
+            {
+                for (int i = firstNeg + 1; i < lastNeg; i++)
+                {
+                    sumBetween += arr[i];
+                }
+            }
+
+            // Стиснення масиву
+            List<double> newArr = new List<double>();
+
+            foreach (double x in arr)
+            {
+                if (Math.Abs(x) > 1)
+                    newArr.Add(x);
+            }
+
+            // Заповнення нулями
+            while (newArr.Count < arr.Length)
+            {
+                newArr.Add(0);
+            }
+
+            // Вивід
+            textBoxResult.Text =
+                $"Сума непарних індексів: {sumOddIndex}\r\n" +
+                $"Сума між від’ємними: {sumBetween}\r\n" +
+                $"Новий масив: {string.Join(" ", newArr)}";
+        }
+    }
+}
